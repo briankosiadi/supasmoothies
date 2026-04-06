c154b2407cfb24a39b1876fcdcd17f44e9ec6842
Brian Kosiadi <briankosiadi@gmail.com>
2026-04-05 23:30:52 -0700

    Complete Lesson 4

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,19 +1,39 @@
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import supabase from "../config/supabaseClient"
+
 const Create = () => {
+  const navigate = useNavigate()
   const [title, setTitle] = useState('')
   const [method, setMethod] = useState('')
   const [rating, setRating] = useState('')
   const [formError, setFormError] = useState(null)
 
   const handleSubmit = async (e) => {
-    e/reventDefault()
+    e.preventDefault()
 
     if (!title || !method || !rating) {
       setFormError('Please fill in all the fields correctly')
       return
     }
+
+    const { data, error } = await supabase
+  .from('smoothies')
+  .insert([{ title, method, rating }])
+  .select()
+      
+    if (error) {
+      console.log(error)
+      setFormError('Please fill in all the fields correctly')
+    }
+    if (data) {
+      console.log(data)
+      setFormError(null)
+      navigate('/')
+    }
   }
 
-return (
+  return (
     <div className="page create">
       <form onSubmit={handleSubmit}>
         <label htmlFor="title">Title:</label>
