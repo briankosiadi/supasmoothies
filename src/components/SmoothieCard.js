e537ae2e8dbc9394e2a0c5dd5a1ebabc874b24dc
Brian Kosiadi <briankosiadi@gmail.com>
2026-04-13 20:26:39 -0700

    Fix Lesson 8

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,36 +1,36 @@
+import supabase from "../config/supabaseClient"
 import { Link } from 'react-router-dom'
-import supabase from '../config/supabaseClient'
 
-const SmoothieCard = ({ smoothie, onDelete }) => {
-    console.log(onDelete)
-    const handleDelete = async () => {
-        const { data, error } = await supabase
-            .from('smoothies')
-            .delete()
-            .eq('id', smoothie.id)
-            .select()
+const SmoothieCard = ({ smoothie, setSmoothies }) => {
 
-        if (error) {
-            console.log(error)
-        }
-        if (data) {
-            console.log(data)
-            onDelete(smoothie.id)
-        }
-    }
-    return (
-        <div className="smoothie-card">
-            <h3>{smoothie.title}</h3>
-            <p>{smoothie.method}</p>
-            <div className="rating">{smoothie.rating}</div>
-            <div className="buttons">
-                <Link to={'/' + smoothie.id}>
-                <i className="material-icons">edit</i>
-                </Link>
-                <i className="material-icons" onClick={handleDelete}>delete</i>
-            </div>
-        </div>
-    )
+const handleDelete = async () => {
+  const { error } = await supabase
+    .from('smoothies')
+    .delete()
+    .eq('id', smoothie.id)
+
+  if (error) {
+    console.log(error)
+  }
+
+  if (!error) {
+    setSmoothies(prev => prev.filter(s => s.id !== smoothie.id))
+  }
+}
+
+  return (
+    <div className="smoothie-card">
+      <h3>{smoothie.title}</h3>
+      <p>{smoothie.method}</p>
+      <div className="rating">{smoothie.rating}</div>
+      <div className="buttons">
+        <Link to={"/" + smoothie.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+        <i className="material-icons" onClick={handleDelete}>delete</i>
+      </div>
+    </div>
+  )
 }
 
 export default SmoothieCard
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,27 @@
-import supabase from "../config/supabaseClient"
+import supabase from '../config/supabaseClient'
 import { useEffect, useState } from 'react'
 
-//components
-import SmoothieCard from "../components/SmoothieCard"
+// components
+import SmoothieCard from '../components/SmoothieCard'
 
 const Home = () => {
   const [fetchError, setFetchError] = useState(null)
   const [smoothies, setSmoothies] = useState(null)
-
-  const handleDelete = (id) => {
-    setSmoothies(prevSmoothies => {
-      return prevSmoothies.filter(sm => sm.id !== id)
-    })
-  }
 
   useEffect(() => {
     const fetchSmoothies = async () => {
       const { data, error } = await supabase
         .from('smoothies')
         .select()
-
-        if (error) {
-          setFetchError('Could not fetch the smoothies')
-          setSmoothies(null)
-          console.log(error)
-        }
-        if (data) {
-          setSmoothies(data)
-          setFetchError(null)
-        }
+      
+      if (error) {
+        setFetchError('Could not fetch the smoothies')
+        setSmoothies(null)
+      }
+      if (data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
     }
 
     fetchSmoothies()
@@ -43,12 +36,9 @@ const Home = () => {
           {/* order-by buttons */}
           <div className="smoothie-grid">
             {smoothies.map(smoothie => (
-              <SmoothieCard
-              key={smoothie.id}
-              smoothie={smoothie}
-              onDelete={handleDelete}
-              />
-          ))}
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} setSmoothies={setSmoothies}
+ />
+            ))}
           </div>
         </div>
       )}
