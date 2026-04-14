ddf2611658c0de578825018d4b9ef8d594df93c9
Brian Kosiadi <briankosiadi@gmail.com>
2026-04-14 13:42:56 -0700

    Lesson 9

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,14 @@ import SmoothieCard from '../components/SmoothieCard'
 const Home = () => {
   const [fetchError, setFetchError] = useState(null)
   const [smoothies, setSmoothies] = useState(null)
+  const [orderBy, setOrderBy] = useState('created_at')
 
   useEffect(() => {
     const fetchSmoothies = async () => {
       const { data, error } = await supabase
         .from('smoothies')
         .select()
+        .order(orderBy, {ascending: false})
       
       if (error) {
         setFetchError('Could not fetch the smoothies')
@@ -26,14 +28,20 @@ const Home = () => {
 
     fetchSmoothies()
 
-  }, [])
+  }, [orderBy])
 
   return (
     <div className="page home">
       {fetchError && (<p>{fetchError}</p>)}
       {smoothies && (
         <div className="smoothies">
-          {/* order-by buttons */}
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrderBy('created_at')}>Time Created</button>
+            <button onClick={() => setOrderBy('title')}>Title</button>
+            <button onClick={() => setOrderBy('rating')}>Rating</button>
+            {orderBy}
+          </div>
           <div className="smoothie-grid">
             {smoothies.map(smoothie => (
               <SmoothieCard key={smoothie.id} smoothie={smoothie} setSmoothies={setSmoothies}
